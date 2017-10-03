47f60d402f241e57012855aa536d12970d87e997
Unknown <vaeppe@gmail.com>
2017-10-03 20:43:30 +0200

    APP Upload

---
+++ src/main/java/com/modern/codes/lime/LimeApplication.java
@@ -0,0 +1,12 @@
+package com.modern.codes.lime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimeApplication.class, args);
+	}
+}
