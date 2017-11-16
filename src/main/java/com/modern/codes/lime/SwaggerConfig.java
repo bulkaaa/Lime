880871fdcd9b9fd42c9dd8db0f9295e5b5f0dca4
Klaudia Jarosz <klaudiajarosz1@poczta.onet.pl>
2017-11-16 16:14:15 +0100

    configure swagger auto generated documentation

---
+++ src/main/java/com/modern/codes/lime/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.modern.codes.lime;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * Main configuration class to enable the Swagger UI frontend. It registers all supported controllers and describes the
+ * API.
+ * 
+ * @author jaroszk
+ * 
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    // Swagger UI
+    public static final String SWAGGER_UI_ENDPOINT = "/swagger-ui.html";
+    public static final String SWAGGER_RESOURCES_ENDPOINT = "/swagger-resources";
+    public static final String SWAGGER_API_DOCS_ENDPOINT = "/v2/api-docs";
+
+//    @Value("${info.build.version}")
+    private String apiVersion;
+//    @Value("${info.build.name}")
+    private String apiTitle;
+//    @Value("${info.build.artifact}")
+    private String apiGroupName;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(apiTitle)
+                .description(
+                        "The Lime application provides funcionalities  for ...")
+                                .termsOfServiceUrl("") .version(apiVersion).build();
+    }
+
+}
