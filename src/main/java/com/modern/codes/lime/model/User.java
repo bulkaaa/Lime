13f0aaf5a894af32c98bb6126fb579c4522b1bd8
Maciej Glowala <vaeppe@gmail.com>
2017-11-20 23:31:57 +0100

    Usage of EntityManager. Implemented basic USER. Added Tomcat properties. Remove /out/ directory from Github

--- src/main/java/com/modern/codes/lime/model/User.java
+++ src/main/java/com/modern/codes/lime/model/User.java
@@ -67,13 +67,20 @@ public class User {
             joinedAt = new Date();
         }
     }
+    public String getId()   { return id; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setSurname(String surname) { this.surname = surname; }
+
+    public String getSurname() {
+        return surname;
     }
 
     public String getDescription() {
