90def94554f73e36060cccae384819319bfcdbf7
Maciej Glowala <vaeppe@gmail.com>
2017-11-22 11:02:37 +0100

    code cleanup

--- src/main/java/com/modern/codes/lime/controller/TestController.java
+++ src/main/java/com/modern/codes/lime/controller/TestController.java
@@ -1,13 +1,8 @@
 package com.modern.codes.lime.controller;
 
-import com.modern.codes.lime.ParseTools;
-import com.modern.codes.lime.dao.IUserDAO;
-import com.modern.codes.lime.model.User;
-//import com.modern.codes.lime.service.IUserService;
 import com.modern.codes.lime.pojo.UserPOJO;
 
 import com.modern.codes.lime.service.IUserService;
-import com.modern.codes.lime.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
