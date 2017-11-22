aa8206ab71bc504aad37fdc5f0cc2ddc28fbd6d8
Maciej Glowala <vaeppe@gmail.com>
2017-11-22 10:56:41 +0100

    Added some funtion + checked

--- src/main/java/com/modern/codes/lime/service/UserService.java
+++ src/main/java/com/modern/codes/lime/service/UserService.java
@@ -1,10 +1,14 @@
 package com.modern.codes.lime.service;
 
+import com.modern.codes.lime.ParseTools;
 import com.modern.codes.lime.dao.IUserDAO;
 import com.modern.codes.lime.model.User;
 import com.modern.codes.lime.pojo.UserPOJO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class UserService extends BasicCRUDService<User, UserPOJO, IUserDAO> implements IUserService {
 
@@ -12,5 +16,8 @@ public class UserService extends BasicCRUDService<User, UserPOJO, IUserDAO> impl
     IUserDAO dao;
     public UserService() {
         super(User.class, UserPOJO.class);
+    }
+    public List<UserPOJO> getUserByNameAndSurname(String name, String surname){
+        return ParseTools.parseList(dao.getUserByNameAndSurname(name, surname), UserPOJO.class);
     }
 }
