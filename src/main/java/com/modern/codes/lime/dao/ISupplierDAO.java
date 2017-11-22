bd7f781bea979efa71e9af1ab538f3d78736f37c
Maciej Glowala <vaeppe@gmail.com>
2017-11-22 23:23:11 +0100

    Implemented Supplier DAO methods

--- src/main/java/com/modern/codes/lime/dao/ISupplierDAO.java
+++ src/main/java/com/modern/codes/lime/dao/ISupplierDAO.java
@@ -3,7 +3,20 @@ package com.modern.codes.lime.dao;
 import com.modern.codes.lime.model.Supplier;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ISupplierDAO extends IBasicCRUDRepository<Supplier, String>{
+    List<Supplier> getByEmailAddress(String emailAddress);
+
+    List<Supplier> getByPostalCode(String postalCode);
+
+    List<Supplier> getByCity(String city);
+
+    List<Supplier> getByCountry(String country);
+
+    List<Supplier> getByName(String name);
+
+    List<Supplier> getByTelephone(String telephone);
 }
