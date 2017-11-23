0f0db3010955634e49ed4db2dbf13e1ba0e05c1a
Maciej Glowala <vaeppe@gmail.com>
2017-11-23 20:48:32 +0100

    Added PrivilegeList filters

--- src/main/java/com/modern/codes/lime/service/IPrivilegeService.java
+++ src/main/java/com/modern/codes/lime/service/IPrivilegeService.java
@@ -1,6 +1,7 @@
 package com.modern.codes.lime.service;
 
 import com.modern.codes.lime.pojo.PrivilegePOJO;
+import com.modern.codes.lime.pojo.RolePOJO;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,5 +24,17 @@ public interface IPrivilegeService {
     }
     static Stream<PrivilegePOJO> filterByName(Stream<PrivilegePOJO> stream, String name){
         return stream.filter(t -> t.getName().equals(name));
+    }
+    static List<PrivilegePOJO> filterByRole(List<PrivilegePOJO> list, String roleName){
+        return filterByRole(list.stream(), roleName).collect(Collectors.toList());
+    }
+    static Stream<PrivilegePOJO> filterByRole(Stream<PrivilegePOJO> stream, String roleName){
+        return stream.filter(t -> !IRoleService.filterByName(t.getRoles(), roleName).isEmpty());
+    }
+    static List<PrivilegePOJO> filterByRole(List<PrivilegePOJO> list, RolePOJO role){
+        return filterByRole(list.stream(), role).collect(Collectors.toList());
+    }
+    static Stream<PrivilegePOJO> filterByRole(Stream<PrivilegePOJO> stream, RolePOJO role){
+        return stream.filter(t -> t.getRoles().contains(role));
     }
 }
