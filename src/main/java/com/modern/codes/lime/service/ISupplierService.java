a84e22de69f3a395824966f74ffea4a64b379b0d
Maciej Glowala <vaeppe@gmail.com>
2017-11-23 21:21:34 +0100

    Added SupplierList filters

--- src/main/java/com/modern/codes/lime/service/ISupplierService.java
+++ src/main/java/com/modern/codes/lime/service/ISupplierService.java
@@ -1,5 +1,6 @@
 package com.modern.codes.lime.service;
 
+import com.modern.codes.lime.pojo.ResourcePOJO;
 import com.modern.codes.lime.pojo.SupplierPOJO;
 
 import java.util.List;
@@ -23,5 +24,38 @@ public interface ISupplierService {
     }
     static Stream<SupplierPOJO> filterByName(Stream<SupplierPOJO> stream, String name){
         return stream.filter(t -> t.getName().equals(name));
+    }
+    static List<SupplierPOJO> filterByCity(List<SupplierPOJO> list, String city){
+        return filterByCity(list.stream(), city).collect(Collectors.toList());
+    }
+    static Stream<SupplierPOJO> filterByCity(Stream<SupplierPOJO> stream, String city){
+        return stream.filter(t -> t.getCity().equals(city));
+    }
+    static List<SupplierPOJO> filterByEmailAddress(List<SupplierPOJO> list, String emailAddress){
+        return filterByEmailAddress(list.stream(), emailAddress).collect(Collectors.toList());
+    }
+    static Stream<SupplierPOJO> filterByEmailAddress(Stream<SupplierPOJO> stream, String emailAddress){
+        return stream.filter(t -> t.getEmailAddress().equals(emailAddress));
+    }
+    static List<SupplierPOJO> filterByTelephone(List<SupplierPOJO> list, String telephone){
+        return filterByTelephone(list.stream(), telephone).collect(Collectors.toList());
+    }
+    static Stream<SupplierPOJO> filterByTelephone(Stream<SupplierPOJO> stream, String telephone){
+        return stream.filter(t -> t.getTelephone().equals(telephone));
+    }
+    static List<SupplierPOJO> filterByCountry(List<SupplierPOJO> list, String country){
+        return filterByCountry(list.stream(), country).collect(Collectors.toList());
+    }
+    static Stream<SupplierPOJO> filterByCountry(Stream<SupplierPOJO> stream, String country){
+        return stream.filter(t -> t.getCountry().equals(country));
+    }
+    static Stream<SupplierPOJO> filterByResource(Stream<SupplierPOJO> stream, String ResourceName){
+        return stream.filter(t -> !IResourceService.filterByName(t.getResources(), ResourceName).isEmpty());
+    }
+    static List<SupplierPOJO> filterByResource(List<SupplierPOJO> list, ResourcePOJO resource){
+        return filterByResource(list.stream(), resource).collect(Collectors.toList());
+    }
+    static Stream<SupplierPOJO> filterByResource(Stream<SupplierPOJO> stream, ResourcePOJO resource){
+        return stream.filter(t -> t.getResources().contains(resource));
     }
 }
