a2dc6781b383341fe62641588791622cfd8f19e3
Maciej Glowala <vaeppe@gmail.com>
2017-11-23 21:13:33 +0100

    Added ResourceList filters

--- src/main/java/com/modern/codes/lime/service/ISupplierService.java
+++ src/main/java/com/modern/codes/lime/service/ISupplierService.java
@@ -3,6 +3,8 @@ package com.modern.codes.lime.service;
 import com.modern.codes.lime.pojo.SupplierPOJO;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface ISupplierService {
     List<SupplierPOJO> findAll();
@@ -16,4 +18,10 @@ public interface ISupplierService {
     void delete(Object t);
     void save(List l);
     void delete(List l);
+    static List<SupplierPOJO> filterByName(List<SupplierPOJO> list, String name){
+        return filterByName(list.stream(), name).collect(Collectors.toList());
+    }
+    static Stream<SupplierPOJO> filterByName(Stream<SupplierPOJO> stream, String name){
+        return stream.filter(t -> t.getName().equals(name));
+    }
 }
