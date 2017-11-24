4599cbf6759b3ab080448f0687adcbfee691d482
Maciej Glowala <vaeppe@gmail.com>
2017-11-24 04:21:30 +0100

    in progress - save to prepare to change the database

--- src/main/java/com/modern/codes/lime/DBPopulator.java
+++ src/main/java/com/modern/codes/lime/DBPopulator.java
@@ -1,13 +1,12 @@
 package com.modern.codes.lime;
 
-import com.modern.codes.lime.model.Resource;
-import com.modern.codes.lime.pojo.ResourcePOJO;
-import com.modern.codes.lime.pojo.SupplierPOJO;
+import com.modern.codes.lime.pojo.*;
 import com.modern.codes.lime.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class DBPopulator {
@@ -38,15 +37,50 @@ public class DBPopulator {
         this.supplierService = supplierService;
         this.userService = userService;
     }
+    private SupplierPOJO supA = new SupplierPOJO();
+    private SupplierPOJO supB = new SupplierPOJO();
+    private UserPOJO userA = new UserPOJO();
+    private UserPOJO userB = new UserPOJO();
+    private UserPOJO userC = new UserPOJO();
+    private RolePOJO roleA = new RolePOJO();
+    private RolePOJO roleB = new RolePOJO();
+    private RolePOJO roleC = new RolePOJO();
+    private ResourcePOJO resourceA = new ResourcePOJO();
+    private ResourcePOJO resourceB = new ResourcePOJO();
+    private ResourcePOJO resourceC = new ResourcePOJO();
+    private ResourcePOJO resourceD = new ResourcePOJO();
+    private ResourcePOJO resourceE = new ResourcePOJO();
+    private ResourcePOJO resourceF = new ResourcePOJO();
+    private ResourcePOJO resourceG = new ResourcePOJO();
+    private ProductPOJO productA = new ProductPOJO();
+    private ProductPOJO productB = new ProductPOJO();
+    private ProductPOJO productC = new ProductPOJO();
+    private PrivilegePOJO privilegeA = new PrivilegePOJO();
+    private PrivilegePOJO privilegeB = new PrivilegePOJO();
+    private PrivilegePOJO privilegeC = new PrivilegePOJO();
+    private JobPOJO jobA = new JobPOJO();
+    private JobPOJO jobB = new JobPOJO();
+    private JobPOJO jobC = new JobPOJO();
+    private JobPOJO jobD = new JobPOJO();
+    private JobPOJO jobE = new JobPOJO();
+    private FormulaPOJO formulaA = new FormulaPOJO();
+    private FormulaPOJO formulaB = new FormulaPOJO();
+    private FormulaPOJO formulaC = new FormulaPOJO();
+    private FormulaPOJO formulaD = new FormulaPOJO();
+    private FormulaPOJO formulaE = new FormulaPOJO();
+    private FormulaPOJO formulaF = new FormulaPOJO();
+    private FormulaPOJO formulaG = new FormulaPOJO();
+    private FormulaPOJO formulaH = new FormulaPOJO();
+    private FormulaPOJO formulaI = new FormulaPOJO();
+    private FormulaPOJO formulaJ = new FormulaPOJO();
+    private FormulaPOJO formulaK = new FormulaPOJO();
+    private FormulaPOJO formulaL = new FormulaPOJO();
 
-    SupplierPOJO supA = new SupplierPOJO();
-    SupplierPOJO supB = new SupplierPOJO();
+
 
     public void populate(){
         clearDB();
-        //SUPPLIER
-
-
+        setSuppliers();
     }
     public void clearDB(){
         jobService.deleteAll();
@@ -73,14 +107,46 @@ public class DBPopulator {
         supB.setCity("Kurza Stopka");
         supA.setEmailAddress("Jan@Kowalski.pl");
         supB.setEmailAddress("Cezary@Pazura.co");
-//        supA.setResources();
-//        supA.setResources();
+        supA.setId(UUID.randomUUID().toString());
+        supA.setId(UUID.randomUUID().toString());
     }
-//    List<ResourcePOJO> AddResources(){
-//        ResourcePOJO milk;
-//        ResourcePOJO flour;
-//        ResourcePOJO egg;
-//        ResourcePOJO sugar;
-//        milk.set
-//    }
+    private void setUser(){
+        userA.setPassword("passA");
+        userA.setLogin("loginA");
+        userA.setDescription("Desctiprion of user B");
+        userA.setJoinedAt(new Date(2017, 5, 5));
+        userA.setName("Maciej");
+        userA.setSurname("Glowala");
+        userA.setId(UUID.randomUUID().toString());
+        userB.setPassword("passB");
+        userB.setLogin("loginB");
+        userB.setDescription("Desctiprion of user B");
+        userB.setJoinedAt(new Date(2017, 6, 6));
+        userB.setName("Klaudia");
+        userB.setSurname("Jarosz");
+        userB.setId(UUID.randomUUID().toString());
+        userC.setPassword("passC");
+        userC.setLogin("loginC");
+        userC.setDescription("Desctiprion of user B");
+        userC.setJoinedAt(new Date(2017, 7, 7));
+        userC.setName("Aleksandra");
+        userC.setSurname("Bulka");
+        userC.setId(UUID.randomUUID().toString());
+    }
+    private void setRole(){
+        roleA.setName("Administrator");
+        roleA.setId(UUID.randomUUID().toString());
+        roleB.setName("Manager");
+        roleB.setId(UUID.randomUUID().toString());
+        roleC.setName("Staff");
+        roleC.setId(UUID.randomUUID().toString());
+    }
+    private void setResources(){
+//        resourceA.setName();
+//        resourceA.setUnit();
+//        resourceA.setQuantity();
+//        resourceA.setId();
+//        resourceA.setDescription();
+//        resourceA.setCategory();
+    }
 }
