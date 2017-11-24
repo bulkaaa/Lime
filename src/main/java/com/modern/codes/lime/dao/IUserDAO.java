06c931726e21dc903e800f97ea685c4818c8f78a
Maciej Glowala <vaeppe@gmail.com>
2017-11-24 03:50:30 +0100

    added method names in UserDAO

--- src/main/java/com/modern/codes/lime/dao/IUserDAO.java
+++ src/main/java/com/modern/codes/lime/dao/IUserDAO.java
@@ -2,8 +2,14 @@ package com.modern.codes.lime.dao;
 import com.modern.codes.lime.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 @Repository
 public interface IUserDAO extends IBasicCRUDRepository<User, String>{
-    List<User> getUserByNameAndSurname(String name, String surname);
+    List<User> getUserByName(String name, String surname);
+    List<User> getUserBySurname(String name, String surname);
+    List<User> getUserByLogin(String name, String surname);
+    List<User> getJoinedBefore(Date date);
+    List<User> getJoinedAfter(Date date);
+    
 }
