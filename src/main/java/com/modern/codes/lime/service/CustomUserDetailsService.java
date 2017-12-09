a79e6098fd527258b303f4a782e90783f574bd50
Unknown <vaeppe@gmail.com>
2017-12-09 12:46:11 +0100

    Yet another AuthService

---
+++ src/main/java/com/modern/codes/lime/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.modern.codes.lime.service;
+
+import com.modern.codes.lime.dao.IUserDAO;
+import com.modern.codes.lime.model.CustomUserDetails;
+import com.modern.codes.lime.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private IUserDAO dao;
+    @Autowired
+    public CustomUserDetailsService(IUserDAO dao){
+        this.dao = dao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = dao.findByUsername(username);
+
+        return new CustomUserDetails(user);
+    }
+}
