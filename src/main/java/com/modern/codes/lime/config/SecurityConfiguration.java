df3413b5103840566f0d138abf7a46d159697071
Unknown <vaeppe@gmail.com>
2017-12-09 14:55:00 +0100

    Set Login Handlers

--- src/main/java/com/modern/codes/lime/config/SecurityConfiguration.java
+++ src/main/java/com/modern/codes/lime/config/SecurityConfiguration.java
@@ -5,12 +5,19 @@ import com.modern.codes.lime.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
@@ -38,10 +45,47 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/dev/hello-world").authenticated()
+                .antMatchers("/dev/**").hasAnyRole("ROLE_ADMIN", "ROLE_MANAGER")
+                .antMatchers("/dev/hello-world2").hasAnyRole("ROLE_STAFF")
+                .antMatchers("/dev/hello-world").hasAnyRole("ROLE_MANAGER")
                 .anyRequest().permitAll()
                 .and()
-                .formLogin().permitAll();
+                .formLogin().loginPage("/login")
+                .successHandler(successHandler())
+                .failureHandler(failureHandler())
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler())
+                .authenticationEntryPoint(authenticationEntryPoint());
+    }
+    private AuthenticationSuccessHandler successHandler() {
+        return (httpServletRequest, httpServletResponse, authentication) -> {
+            httpServletResponse.getWriter().append("successful operation");
+            httpServletResponse.setIntHeader("X-Rate-Limit", 1000);
+            httpServletResponse.addDateHeader("X-Expires-After", System.currentTimeMillis() + 7200000);
+            httpServletResponse.setStatus(200);
+        };
+    }
+
+    private AuthenticationFailureHandler failureHandler() {
+        return (httpServletRequest, httpServletResponse, e) -> {
+            httpServletResponse.getWriter().append("Authentication failure");
+            httpServletResponse.setStatus(401);
+        };
+    }
+
+    private AccessDeniedHandler accessDeniedHandler() {
+        return (httpServletRequest, httpServletResponse, e) -> {
+            httpServletResponse.getWriter().append("Access denied");
+            httpServletResponse.setStatus(403);
+        };
+    }
+
+    private AuthenticationEntryPoint authenticationEntryPoint() {
+        return (httpServletRequest, httpServletResponse, e) -> {
+            httpServletResponse.getWriter().append("Not authenticated");
+            httpServletResponse.setStatus(401);
+        };
     }
 
     private PasswordEncoder getPasswordEncoder() {
