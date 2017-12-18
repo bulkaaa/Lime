3a8fd7669482b28158905d4d1f391fb1cf639b89
jaroszk <klaudiajarosz1@poczta.onet.pl>
2017-12-18 10:30:12 +0100

    basic registration and login logic updated

--- src/main/webapp/cart/CartController.js
+++ src/main/webapp/cart/CartController.js
@@ -1,6 +1,16 @@
 
 app.controller('CartController', function($scope) {
 
+     $scope.items = [
+                {name: 'resource1', value: '6', description:'resource1 description', image: "libs/img/BlueTestTube.jpg"},
+                {name: 'resource2', value: '56', description:'resource2 description', image: "libs/img/OrangeTestTube.png"},
+                {name: 'resource3', value: '98', description:'resource3 description', image: "libs/img/PinkTestTube.jpg"},
+                {name: 'resource4', value: '54', description:'resource4 description', image: "libs/img/BlueTestTube.jpg"},
+                {name: 'resource5', value: '7', description:'resource5 description', image: "libs/img/OrangeTestTube.png"},
+                {name: 'resource6', value: '30', description:'resource6 description', image: "libs/img/PinkTestTube.jpg"}
+              ];
+
+$(document).ready(function(){
     $('.minus-btn').on('click', function(e) {
             e.preventDefault();
             var $this = $(this);
@@ -25,12 +35,23 @@ app.controller('CartController', function($scope) {
             $input.val(value);
         });
 
-        $('.like-btn').on('click', function() {
-            $(this).toggleClass('is-active');
-        });
 
         $('.delete-btn').on('click', function() {
             $(this).closest('.item').remove();
         });
+    })
+
+
+        $scope.open = function() {
+            $scope.showModal = true;
+          };
+
+          $scope.ok = function() {
+            $scope.showModal = false;
+          };
+
+          $scope.cancel = function() {
+            $scope.showModal = false;
+          };
 
     });
