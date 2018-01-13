f8939bc45b1f00f0ed993c35b57c1bb16ce149dd
Maciej_Glowala <vaeppe@gmail.com>
2018-01-14 00:10:56 +0100

    Order refactor

--- src/main/java/com/modern/codes/lime/order/Order.java
+++ src/main/java/com/modern/codes/lime/order/Order.java
@@ -10,6 +10,9 @@ import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
+import com.modern.codes.lime.model.Resource;
+import com.modern.codes.lime.pojo.ResourcePOJO;
+
 @Service
 public class Order {
 
@@ -18,42 +21,37 @@ public class Order {
     }
 
 
-    public static String ConstructOrderMsg(String SupplierName, String ResourceName, Integer Amount){
-       return "Dear " + SupplierName + ", " + "\n\n We would like to order " + Amount.toString() + " units of " + ResourceName + " for our laboratory. \n\n";
+    public static String ConstructOrderMsg(final String supplierName, final Map<Resource, Integer> map){
+        final StringBuilder message = new StringBuilder("Dear " + supplierName + ", " + "\n\n We would like to order: \n");
+        map.forEach((key, value) -> message.append(key.getName())
+                                           .append(" : ")
+                                           .append(value)
+                                           .append(' ')
+                                           .append(key
+                                                    .getUnit())
+                                           .append('\n'));
+
+        message.append("Regards, \nLIME team.\n");
+        return message.toString();
 
     }
-    public static void SendEmail(String to, String subject, String content, String attachment){
-        final String username = "lime.lab.application@gmail.com";
-        final String password = "lime1234";
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-
+    public static void SendEmail(final String to, final String subject, final String content, final String attachment){
+        final Session session = getSession();
         try {
 
-            Message message = new MimeMessage(session);
+            final Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("lime.lab.application@gmail.com"));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(to));
             message.setSubject(subject);
 
-            Multipart multipart = new MimeMultipart();
+            final Multipart multipart = new MimeMultipart();
 
-            MimeBodyPart textBodyPart = new MimeBodyPart();
+            final MimeBodyPart textBodyPart = new MimeBodyPart();
             textBodyPart.setText(content);
 
-            MimeBodyPart attachmentBodyPart= new MimeBodyPart();
-            DataSource source = new FileDataSource(attachment);
+            final MimeBodyPart attachmentBodyPart= new MimeBodyPart();
+            final DataSource source = new FileDataSource(attachment);
             attachmentBodyPart.setDataHandler(new DataHandler(source));
             attachmentBodyPart.setFileName(source.getName());
 
@@ -63,32 +61,16 @@ public class Order {
             message.setContent(multipart);
             Transport.send(message);
 
-            System.out.println("Done");
 
-        } catch (MessagingException e) {
+        } catch (final MessagingException e) {
             throw new RuntimeException(e);
         }
     }
-    public static void SendEmail(String to, String subject, String content){
-        final String username = "lime.lab.application@gmail.com";
-        final String password = "lime1234";
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-
+    public static void SendEmail(final String to, final String subject, final String content){
+        final Session session = getSession();
         try {
 
-            Message message = new MimeMessage(session);
+            final Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("lime.lab.application@gmail.com"));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(to));
@@ -97,11 +79,29 @@ public class Order {
 
             Transport.send(message);
 
-            System.out.println("Done");
-
-        } catch (MessagingException e) {
+        } catch (final MessagingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Properties getProperties() {
+        final Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        return props;
+    }
+    private static Session getSession(){
+        final Properties props = getProperties();
+        final String username = "lime.lab.application@gmail.com";
+        final String password = "lime1234";
+        return Session.getInstance(props,
+                                   new javax.mail.Authenticator() {
+                                       protected PasswordAuthentication getPasswordAuthentication() {
+                                           return new PasswordAuthentication(username, password);
+                                       }
+                                   });
     }
 }
 
