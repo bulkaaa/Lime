6f6575e9ca95057a99e9ef5920ec55a11d546b40
Maciej_Glowala <vaeppe@gmail.com>
2018-01-14 00:10:56 +0100

    Order refactor

--- src/main/java/com/modern/codes/lime/controller/OrderController.java
+++ src/main/java/com/modern/codes/lime/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.modern.codes.lime.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.modern.codes.lime.model.Resource;
+import com.modern.codes.lime.model.Supplier;
 import com.modern.codes.lime.order.Order;
 import com.modern.codes.lime.service.IResourceService;
 import com.modern.codes.lime.tools.ParseTools;
@@ -46,25 +47,30 @@ public class OrderController {
     }
 
 
-    @RequestMapping(value = "/order", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/send", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Order choosen resources with given amount", notes = "Post choosen List of resource ID "
                                                               +  "Saves it into DB.", response = Boolean.class)
     @ApiResponses(value = { @ApiResponse(code = 200, message = "Resources orderer"),
                             @ApiResponse(code = 422, message = "In case of validation errors")})
     @ResponseBody
-    public Boolean order(
+    public Boolean send(
             @ApiParam(value = "Map of Resources and amout") @RequestBody final @Validated Map<Resource, Integer> orderList,
             BindingResult bindingResult, UriComponentsBuilder b) {
         LOG.info("order request received map resource:amount \n {} ", orderList);
-        orderList.forEach((key, value) -> Order.SendEmail(key.getSupplier().getEmailAddress(), "Order from LIME", Order.ConstructOrderMsg(key.getSupplier().getName(), key.getName(), value)));
+        final Map<Supplier, Map<Resource, Integer>> supplierMap = new HashMap<>();
+        orderList.forEach((key, value) -> {
+            Map<Resource, Integer> suppVal = supplierMap.get(key.getSupplier());
+            if(null != suppVal){
+                suppVal.put(key,value);
+                //supplierMap.put(key.getSupplier(), suppVal);
+            }
+            else{
+                suppVal = new HashMap<>();
+                suppVal.put(key, value);
+                supplierMap.put(key.getSupplier(), suppVal);
+            }
+        });
+          supplierMap.forEach((key, value) -> Order.SendEmail(key.getEmailAddress(), "Order from LIME", Order.ConstructOrderMsg(key.getName(), value)));
         return true;
-    }
-
-    @GetMapping(path = "/test")
-    public String sendTest(){
-        String msg = Order.ConstructOrderMsg("Some Company", "Aloe", 8);
-        Order.SendEmail("aleksandrabulka1@gmail.com", "Order Email form LIME", msg, "C:\\INPUT.csv");
-        Order.SendEmail("aleksandrabulka1@gmail.com", "Order Email form LIME", msg);
-        return "Email Sent";
     }
 }
