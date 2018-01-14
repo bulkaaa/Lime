cfb9586fb835e53b1ba9b12b96c3e5f60fbedcd4
Maciej_Glowala <vaeppe@gmail.com>
2018-01-14 02:28:06 +0100

    Refactor DrawSeries

--- src/main/java/com/modern/codes/lime/report/DrawSeries.java
+++ src/main/java/com/modern/codes/lime/report/DrawSeries.java
@@ -2,76 +2,66 @@ package com.modern.codes.lime.report;
 
 import org.knowm.xchart.*;
 import org.knowm.xchart.style.Styler;
-import org.knowm.xchart.style.markers.SeriesMarkers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-@Service
+
 public class DrawSeries {
 
-    @Autowired
-    public DrawSeries() {
-    }
+    public static String plot(final ArrayList<TimeSeries> timeSeriesList,
+                              final ArrayList<TimeSeries> timeSeriesForecastList, final Date toDay) {
+        final XYChart chart = new XYChartBuilder().width(600)
+                                                  .height(400)
+                                                  .title("TimeSeries")
+                                                  .xAxisTitle("Time")
+                                                  .yAxisTitle("Production")
+                                                  .build();
+        chart.getStyler()
+             .setLegendPosition(Styler.LegendPosition.OutsideE);
+        chart.getStyler()
+             .setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
+        chart.getStyler()
+             .setDatePattern("dd-MMM-YY");
+        final Date firstDay = getDate(timeSeriesList, toDay);
 
-    public static String plot(ArrayList<TimeSeries> timeSeriesList, ArrayList<TimeSeries> timeSeriesForecastList, Date toDay) {
-        XYChart chart = new XYChartBuilder().width(600).height(400).title("TimeSeries").xAxisTitle("Time").yAxisTitle("Production").build();
-
-        chart.getStyler().setLegendPosition(Styler.LegendPosition.OutsideE);
-        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
-        chart.getStyler().setDatePattern("dd-MMM-YY");
-        Calendar call = Calendar.getInstance();
-        call.setTime(toDay);
-        call.add(Calendar.DATE, -timeSeriesList.get(0).size()+ 1);
-        Date firstDay = call.getTime();
-
-        for (TimeSeries timeSeries: timeSeriesForecastList) {
-            List<Date> xData = new ArrayList<Date>();
-            List<Integer> yData = new ArrayList<Integer>();
-            for (int i = 0; i< timeSeries.size(); i++){
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(firstDay);
-                cal.add(Calendar.DATE, +i);
-                Date dateBefore = cal.getTime();
-                xData.add(dateBefore);
-                yData.add(timeSeries.get(i));
-            }
-            XYSeries series = chart.addSeries(timeSeries.getLabel(), xData, yData);
-
-        }
-        for (TimeSeries timeSeries: timeSeriesList) {
-            List<Date> xData = new ArrayList<Date>();
-            List<Integer> yData = new ArrayList<Integer>();
-            for (int i = 0; i< timeSeries.size(); i++){
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(firstDay);
-                cal.add(Calendar.DATE, +i);
-                Date dateBefore = cal.getTime();
-                xData.add(dateBefore);
-                yData.add(timeSeries.get(i));
-            }
-            XYSeries series = chart.addSeries(timeSeries.getLabel(), xData, yData);
-
-        }
+        ProcessDataInTimeSeries(timeSeriesForecastList, chart, firstDay);
+        ProcessDataInTimeSeries(timeSeriesList, chart, firstDay);
 
         // Save it
-        String filename = "./Sample_Chart";
+        final String filename = "./Sample_Chart";
         try {
             BitmapEncoder.saveBitmap(chart, filename, BitmapEncoder.BitmapFormat.PNG);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         }
-
-        return filename+".png";
+        return filename + ".png";
     }
 
+    private static void ProcessDataInTimeSeries(final ArrayList<TimeSeries> timeSeriesList, final XYChart chart,
+                                                final Date firstDay) {
+        for (final TimeSeries timeSeries : timeSeriesList) {
+            final List<Date> xData = new ArrayList<>();
+            final List<Integer> yData = new ArrayList<>();
+            final Calendar cal = Calendar.getInstance();
+            for (int i = 0; i < timeSeries.size(); i++) {
+                cal.setTime(firstDay);
+                cal.add(Calendar.DATE, +i);
+                final Date dateBefore = cal.getTime();
+                xData.add(dateBefore);
+                yData.add(timeSeries.get(i));
+            }
+            chart.addSeries(timeSeries.getLabel(), xData, yData);
+        }
+    }
+
+    private static Date getDate(final ArrayList<TimeSeries> timeSeriesList, final Date toDay) {
+        final Calendar call = Calendar.getInstance();
+        call.setTime(toDay);
+        call.add(Calendar.DATE, -timeSeriesList.get(0)
+                                                       .size() + 1);
+        return call.getTime();
+    }
 }
