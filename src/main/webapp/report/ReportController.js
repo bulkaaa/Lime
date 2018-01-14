52d5ec555306af9dff40843af2ee4edc9985ee3f
jaroszk <klaudiajarosz1@poczta.onet.pl>
2018-01-14 22:44:14 +0100

    report view

--- src/main/webapp/report/ReportController.js
+++ src/main/webapp/report/ReportController.js
@@ -1,4 +1,4 @@
-app.controller('ReportController', ['$scope', '$http', '$uibModal', function($scope, $http, $modal) {
+app.controller('ReportController', ['$scope', '$http', function($scope, $http) {
 
 $scope.getProducts = function() {
         $http.get("/product/all")
@@ -14,6 +14,25 @@ $scope.getProducts = function() {
                 }
             );
     };
+
+    $scope.generateReport = function () {
+        var item = {};
+        item.productIds = $scope.products;
+        item.email = $scope.email;
+        item.date = new Date($scope.date);
+        item.noDays = $scope.noDays;
+
+        $http.post("/resource/generate", JSON.stringify(item))
+            .then(
+                function(response){
+                    if (response.data){
+                    }
+                },
+                function(response){
+                    alert( "failure message: " + JSON.stringify(response));
+                }
+            );
+    };
   $scope.checkAll = function() {
     $scope.user.roles = $scope.roles.map(function(item) { return item.id; });
   };
@@ -24,7 +43,9 @@ $scope.getProducts = function() {
     $scope.user.roles.splice(0, $scope.user.roles.length);
     $scope.user.roles.push(1);
   };
-
+    $scope.list = {
+        products: []
+    };
 $scope.getProducts();
 
 
