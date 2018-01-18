394eec9b631ce89907fc4c8b7e9a48474d50ef7c
Klaudia Jarosz <klaudiajarosz1@poczta.onet.pl>
2018-01-18 21:29:23 +0100

    refactor report & forecast controllers; display graphs on view

--- src/main/java/com/modern/codes/lime/controller/ForecastController.java
+++ src/main/java/com/modern/codes/lime/controller/ForecastController.java
@@ -1,18 +1,15 @@
 package com.modern.codes.lime.controller;
 
 import com.modern.codes.lime.exception.InvalidRequestException;
-import com.modern.codes.lime.model.Product;
 import com.modern.codes.lime.order.Order;
 import com.modern.codes.lime.report.*;
 import com.modern.codes.lime.service.IJobService;
-import com.modern.codes.lime.service.IProductService;
-import com.modern.codes.lime.tools.ParseTools;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,19 +18,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(path="/forecast")
@@ -43,15 +34,15 @@ public class ForecastController extends BaseController{
     @Autowired
     IJobService jobService;
 
+
     @RequestMapping(value = "/generate", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Boolean generateForecast(
-            @RequestBody @ApiParam(value = "Map of Resources and amount") @Validated final List<String> productIds,
+    public ResponseEntity<byte[]>  generateForecast(
+            @RequestBody @ApiParam(value = "List of product ids") @Validated final List<String> productIds,
             @RequestParam final String startDate,
             @RequestParam final Integer noDays,
-            @RequestParam final Integer noDaysForecast,
-            @RequestParam final String email) {
-        LOG.info("Send request received product list: \n Date from: {} \n noDays: [] \n noDaysForecast: [] \n email: {} \n", productIds, startDate, noDays, noDaysForecast, email);
+            @RequestParam final Integer noDaysForecast) {
+        LOG.info("Generate request received product list: \n Date from: {} \n noDays: [] \n noDaysForecast: [] \n", productIds, startDate, noDays, noDaysForecast);
 
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
         Date date;
@@ -68,12 +59,41 @@ public class ForecastController extends BaseController{
             forecast.setLabel(series.getLabel() + " Forecast");
             seriesFL.add(forecast);
         }
-        final String filename = DrawSeries.plot(seriesL, seriesFL, date, "Production in the Past " + noDays + " Days and forecast for the next " + noDaysForecast + " days.");
-        Order.SendEmail(email,"Forecast Email from LIME", "Please Find Report Attached", filename);
+        byte[] bytes = DrawSeries.plot(seriesL, seriesFL, date, "Production in the Past " + noDays + " Days and forecast for the next " + noDaysForecast + " days.", "Sample_Chart");
 
-        return true;
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(bytes);
     }
 
+    @RequestMapping(value = "/send", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public void  sendForecast(
+            @RequestBody @ApiParam(value = "List of product ids") @Validated final List<String> productIds,
+            @RequestParam final String startDate,
+            @RequestParam final Integer noDays,
+            @RequestParam final Integer noDaysForecast,
+            @RequestParam final String email) {
+        LOG.info("Send request received product list: \n Date from: {} \n noDays: [] \n noDaysForecast: [] \n email: {} \n", productIds, startDate, noDays, noDaysForecast, email);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        Date date;
+        try {
+            date = formatter.parse(startDate);
+        } catch (ParseException e) {
+            throw new InvalidRequestException("Invalid date format.", null, Locale.ENGLISH);
+        }
+        final ArrayList<TimeSeries> seriesL = TimeSeriesProduct.Extract(jobService, date, noDays);
+        final ArrayList<TimeSeries> seriesFL = new ArrayList<>();
+        for (final TimeSeries series : seriesL) {
+            final TimeSeries forecast = Smoothing.calculateSmoothing(series, noDaysForecast);
+            forecast.setLabel(series.getLabel() + " Forecast");
+            seriesFL.add(forecast);
+        }
+        DrawSeries.plot(seriesL, seriesFL, date, "Production in the Past " + noDays + " Days and forecast for the next " + noDaysForecast + " days.", "Sample_Chart");
+        Order.SendEmail(email,"Forecast Email from LIME", "Please Find Report Attached", "Sample_Chart");
+    }
 
     @GetMapping(path = "/test")
     public String Test() {
@@ -88,10 +108,10 @@ public class ForecastController extends BaseController{
             seriesFL.add(fcst);
         }
 
-        final String filename = DrawSeries.plot(seriesL, seriesFL, new Date(), "Production in the Past" + days_past + "Days and forecast for the next " + days_forecast);
+        //final String filename = DrawSeries.plot(seriesL, seriesFL, new Date(), "Production in the Past" + days_past + "Days and forecast for the next " + days_forecast);
 
         final String email = "aleksandrabulka1@gmail.com";
-        Order.SendEmail(email, "Report Email form LIME", "Please Find Report Attached", filename);
+        //Order.SendEmail(email, "Report Email form LIME", "Please Find Report Attached", filename);
 
         return "Email Sent";
     }
--- src/main/webapp/prediction/PredictionController.js
+++ src/main/webapp/prediction/PredictionController.js
@@ -20,7 +20,25 @@ $scope.getProducts = function() {
         item = $scope.list.products;
         var date = new Date($scope.date);
 
-        $http.post("/forecast/generate" + "?email=" + $scope.email + "&startDate=" + date.getDate()+"-"+date.getMonth()+1+"-"+date.getFullYear() + "&noDays=" + $scope.noDays + "&noDaysForecast=" + $scope.noDaysForecast, JSON.stringify(item))
+        $http.post("/forecast/generate" + "?startDate=" + date.getDate()+"-"+date.getMonth()+1+"-"+date.getFullYear() + "&noDays=" + $scope.noDays + "&noDaysForecast=" + $scope.noDaysForecast, JSON.stringify(item))
+            .then(
+                function(response){
+                    if (response.data){
+                        $scope.image = response.data;
+                    }
+                },
+                function(response){
+                    alert( "failure message: " + JSON.stringify(response));
+                }
+            );
+    };
+
+    $scope.sendForecast = function () {
+        var item = {};
+        item = $scope.list.products;
+        var date = new Date($scope.date);
+
+        $http.post("/forecast/send" + "?email=" + $scope.email + "&startDate=" + date.getDate()+"-"+date.getMonth()+1+"-"+date.getFullYear() + "&noDays=" + $scope.noDays + "&noDaysForecast=" + $scope.noDaysForecast, JSON.stringify(item))
             .then(
                 function(response){
                     if (response.data){
@@ -31,6 +49,7 @@ $scope.getProducts = function() {
                 }
             );
     };
+
     $scope.list = {
         products: []
     };
