2bd226fd8354dd44c010b339d8c4ea0eab741288
Maciej_Glowala <vaeppe@gmail.com>
2018-01-21 22:03:59 +0100

    getUsedResourcesValues

--- src/main/java/com/modern/codes/lime/controller/DevController.java
+++ src/main/java/com/modern/codes/lime/controller/DevController.java
@@ -52,27 +52,32 @@ public class DevController {
     @GetMapping(path = "/GetResourcesForJob")
     public void resdev(){
         try {
-            final Map<String,Double> valueMap = new HashMap<>();
             final List<String> resourceIds = new ArrayList<>();
             resourceIds.add("4858b21b-75c8-4d01-8076-3434bf9d721c"); //egg
             resourceIds.add("914c96b9-f598-45cd-9406-60ca175b42e5"); //flour
             final Date startDate = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse("January 1, 2018");
             final Date endDate = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse("January 29, 2018");
-            final List<JobPOJO> jobList = jobService.findByEndDateBetween(startDate, endDate);
-            jobList.forEach(job ->{
-                final List<FormulaPOJO> formula = formulaService.findByProductId(job.getPOJOProduct().getId());
-                final List<FormulaPOJO> filteredFormula = IFormulaService.filterByResources(formula, resourceIds);
-                filteredFormula.forEach(y -> {
-                    final ResourcePOJO resource = y.getPOJOResource();
-                    Double value = valueMap.get(resource.getName());
-                    if (null == value)
-                        value = 0.0;
-                    value += y.getValue();
-                    valueMap.put(resource.getName(), value);
-                });
-            });
+            getUsedResourcesValues(startDate, endDate, resourceIds);
         } catch (final ParseException e) {
             e.printStackTrace();
         }
+    }
+    private void getUsedResourcesValues(final Date startDate, final Date endDate, final List<String> resourceIds) {
+
+        final Map<String, Double> valueMap = new HashMap<>();
+        final List<JobPOJO> jobList = jobService.findByEndDateBetween(startDate, endDate);
+        jobList.forEach(job -> {
+            final List<FormulaPOJO> formula = formulaService.findByProductId(job.getPOJOProduct()
+                                                                                .getId());
+            final List<FormulaPOJO> filteredFormula = IFormulaService.filterByResources(formula, resourceIds);
+            filteredFormula.forEach(y -> {
+                final ResourcePOJO resource = y.getPOJOResource();
+                Double value = valueMap.get(resource.getName());
+                if (null == value)
+                    value = 0.0;
+                value += y.getValue();
+                valueMap.put(resource.getName(), value);
+            });
+        });
     }
 }
