7213338a0903c8a14dd36413e0c04524392f2b62
Maciej_Glowala <vaeppe@gmail.com>
2018-01-21 21:09:16 +0100

    Method to get formula from job in dev

--- src/main/java/com/modern/codes/lime/controller/DevController.java
+++ src/main/java/com/modern/codes/lime/controller/DevController.java
@@ -1,5 +1,13 @@
 package com.modern.codes.lime.controller;
 
+import java.util.List;
+
+import com.modern.codes.lime.pojo.FormulaPOJO;
+import com.modern.codes.lime.pojo.JobPOJO;
+import com.modern.codes.lime.pojo.ResourcePOJO;
+import com.modern.codes.lime.service.IFormulaService;
+import com.modern.codes.lime.service.IJobService;
+import com.modern.codes.lime.service.IResourceService;
 import com.modern.codes.lime.service.UserService;
 import com.modern.codes.lime.tools.DBPopulator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +24,10 @@ public class DevController {
     DBPopulator pop;
     @Autowired
     UserService serv;
+    @Autowired
+    IJobService jobService;
+    @Autowired
+    IFormulaService formulaService;
     @GetMapping(path = "/populate")
     public String populate(){
         pop.populate();
@@ -30,5 +42,16 @@ public class DevController {
         return "hello worl2d";
     }
 
+    @GetMapping(path = "/GetResourcesForJob")
+    public void resdev(){
+        final JobPOJO job = jobService.findAll().get(0);
+
+        // get list of ALL Resources to do this job
+        final List<FormulaPOJO> formula = formulaService.findByProductId(job.getId());
+        // get name of FIRST resource needed to do this job
+        String name = formula.get(0).getPOJOResource().getName();
+        // get value needed of FIRST resource to do this job
+        double value = formula.get(0).getValue();
+    }
 
 }
