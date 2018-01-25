36f30e8ffc330f70ee99a4e4a014d8827675c5dd
Maciej_Glowala <vaeppe@gmail.com>
2018-01-25 23:02:19 +0100

    Comment roles at backend

--- src/main/java/com/modern/codes/lime/config/SecurityConfiguration.java
+++ src/main/java/com/modern/codes/lime/config/SecurityConfiguration.java
@@ -45,71 +45,36 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/forecast/**").hasAnyRole("ADMIN", "MANAGER")
-                .antMatchers("/order/**").hasAnyRole("ADMIN", "MANAGER")
-                .antMatchers("/report/**").hasAnyRole("ADMIN", "MANAGER")
-                .antMatchers("/job/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/product/create").hasAnyRole("ADMIN")
-                .antMatchers("/product/update").hasAnyRole("ADMIN")
-                .antMatchers("/product/delete/**").hasAnyRole("ADMIN")
-                .antMatchers("/product/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/product/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/product/get-by-name").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/resource/create").hasAnyRole("ADMIN")
-                .antMatchers("/resource/update").hasAnyRole("ADMIN")
-                .antMatchers("/resource/delete/**").hasAnyRole("ADMIN")
-                .antMatchers("/resource/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/resource/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/supplier/create").hasAnyRole("ADMIN")
-                .antMatchers("/supplier/update").hasAnyRole("ADMIN")
-                .antMatchers("/supplier/delete/**").hasAnyRole("ADMIN")
-                .antMatchers("/supplier/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/supplier/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/user/create").hasAnyRole("ADMIN")
-                .antMatchers("/user/update").hasAnyRole("ADMIN")
-                .antMatchers("/user/delete/**").hasAnyRole("ADMIN")
-                .antMatchers("/user/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/user/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/user/get-by-username/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
-                .antMatchers("/user/get-roles/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/forecast/**").hasAnyRole("ADMIN", "MANAGER")
+                // .antMatchers("/order/**").hasAnyRole("ADMIN", "MANAGER")
+                // .antMatchers("/report/**").hasAnyRole("ADMIN", "MANAGER")
+                // .antMatchers("/job/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/product/create").hasAnyRole("ADMIN")
+                // .antMatchers("/product/update").hasAnyRole("ADMIN")
+                // .antMatchers("/product/delete/**").hasAnyRole("ADMIN")
+                // .antMatchers("/product/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/product/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/product/get-by-name").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/resource/create").hasAnyRole("ADMIN")
+                // .antMatchers("/resource/update").hasAnyRole("ADMIN")
+                // .antMatchers("/resource/delete/**").hasAnyRole("ADMIN")
+                // .antMatchers("/resource/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/resource/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/supplier/create").hasAnyRole("ADMIN")
+                // .antMatchers("/supplier/update").hasAnyRole("ADMIN")
+                // .antMatchers("/supplier/delete/**").hasAnyRole("ADMIN")
+                // .antMatchers("/supplier/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/supplier/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/user/create").hasAnyRole("ADMIN")
+                // .antMatchers("/user/update").hasAnyRole("ADMIN")
+                // .antMatchers("/user/delete/**").hasAnyRole("ADMIN")
+                // .antMatchers("/user/one/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/user/all").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/user/get-by-username/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
+                // .antMatchers("/user/get-roles/**").hasAnyRole("ADMIN", "MANAGER", "STAFF")
                 .anyRequest().permitAll()
                 .and()
-                .formLogin()
-                .successHandler(successHandler())
-                .failureHandler(failureHandler())
-                .and()
-                .exceptionHandling()
-                .accessDeniedHandler(accessDeniedHandler())
-                .authenticationEntryPoint(authenticationEntryPoint());
-    }
-    private AuthenticationSuccessHandler successHandler() {
-        return (httpServletRequest, httpServletResponse, authentication) -> {
-            httpServletResponse.getWriter().append("successful operation");
-            httpServletResponse.setIntHeader("X-Rate-Limit", 1000);
-            httpServletResponse.addDateHeader("X-Expires-After", System.currentTimeMillis() + 7200000);
-            httpServletResponse.setStatus(200);
-        };
-    }
-
-    private AuthenticationFailureHandler failureHandler() {
-        return (httpServletRequest, httpServletResponse, e) -> {
-            httpServletResponse.getWriter().append("Authentication failure");
-            httpServletResponse.setStatus(401);
-        };
-    }
-
-    private AccessDeniedHandler accessDeniedHandler() {
-        return (httpServletRequest, httpServletResponse, e) -> {
-            httpServletResponse.getWriter().append("Access denied");
-            httpServletResponse.setStatus(403);
-        };
-    }
-
-    private AuthenticationEntryPoint authenticationEntryPoint() {
-        return (httpServletRequest, httpServletResponse, e) -> {
-            httpServletResponse.getWriter().append("Not authenticated");
-            httpServletResponse.setStatus(401);
-        };
+                .formLogin();
     }
 
     private PasswordEncoder getPasswordEncoder() {
