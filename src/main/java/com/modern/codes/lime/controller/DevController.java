b1b73973412b23dd733119aefd2ac6ec71b5c996
Maciej_Glowala <vaeppe@gmail.com>
2018-01-26 06:32:08 +0100

    GetActualUserMethod

--- src/main/java/com/modern/codes/lime/controller/DevController.java
+++ src/main/java/com/modern/codes/lime/controller/DevController.java
@@ -1,7 +1,15 @@
 package com.modern.codes.lime.controller;
 
+import com.modern.codes.lime.model.CustomUserDetails;
+import com.modern.codes.lime.model.User;
+import com.modern.codes.lime.pojo.UserPOJO;
+import com.modern.codes.lime.service.IUserService;
 import com.modern.codes.lime.tools.DBPopulator;
+import com.modern.codes.lime.tools.ParseTools;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +22,21 @@ public class DevController {
     @Autowired
     DBPopulator pop;
 
+    @Autowired
+    IUserService userService;
+
     @GetMapping(path = "/populate")
     public String populate(){
         pop.populate();
         return "DB reset completed";
+    }
+    @GetMapping(path = "/act-user")
+    public String getUser(){
+        return ParseTools.parseToJson(getActualUser(), User.class);
+    }
+    private UserPOJO getActualUser(){
+        return userService.findByUsername(((UserDetails) SecurityContextHolder.getContext()
+                                                                                      .getAuthentication()
+                                                                                      .getPrincipal()).getUsername());
     }
 }
