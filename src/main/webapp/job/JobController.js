692fcc1d460000efeab0a5e62884a4b668540113
klaudia.jarosz <klaudiajarosz1@poczta.onet.pl>
2018-03-14 16:50:43 +0100

    "almost" working job view

--- src/main/webapp/job/JobController.js
+++ src/main/webapp/job/JobController.js
@@ -1,3 +1,61 @@
 app.controller('JobController', ['$scope', '$rootScope', '$http', '$uibModal', 'dialogs', 'DialogService', function($scope, $rootScope, $http, $modal, $dialogs, DialogService) {
 
+var modalInstance = null;
+    $scope.job = true;
+
+    $scope.showAll = function() {
+        $http.get("/job/all")
+            .then(
+                function (response) {
+                    if (response.data) {
+                        $scope.items = response.data;
+                        if (!$scope.items.length)
+                            $dialogs.notify('Currently there are no products added in LIME', "You can add products here by clicking on 'Add New Product' button");
+                    }
+                },
+                function (response) {
+                    DialogService.generalServerError();
+                }
+            );
+    };
+
+    $scope.showAll();
+
+
+    $scope.addRecord = function(){
+        $scope.item={};
+        modalInstance = $modal.open({
+            templateUrl: 'modals/add-record.html',
+            controller: 'AddRecordController',
+            scope: $scope,
+            size: '',
+            resolve: {
+            }
+        });
+    };
+
+    $scope.saveRecord = function(item) {
+
+        $http.post("/job/create", JSON.stringify(item))
+            .then(
+                function(response){
+                    if (response.data){
+                        console.log("created PRODUCT successfully!");
+                        item = response.data;
+                        $scope.items.push({
+                            id: item.id,
+                            productName: item.product,
+                            jobDescription: item.details,
+                            resultValue: item.resultValue,
+                            startDate: item.startDate,
+                            endDate: item.endDate,
+                        });
+                    }
+                },
+                function(response){
+                    DialogService.handle(response, 'product', 'create');
+                }
+            );
+    }
+
 }]);
