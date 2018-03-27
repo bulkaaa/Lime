6fd0c1cf5bb19b1eb14aa62fbd5eeaecfe686f5c
Maciej_Glowala <vaeppe@gmail.com>
2018-03-28 01:46:58 +0200

    Refactor + some implementations

---
+++ src/main/webapp/modals/EditFormulaController.js
@@ -0,0 +1,18 @@
+app.controller('AddFormulaController',  ['$scope', '$http','$uibModalInstance', function($scope, $http, modalInstance) {
+    $scope.saveEmp = function () {
+        $scope.newRecord = {};
+
+        $scope.cancelModal();
+
+        console.log(scope.resources)
+        // if($scope.resources && $scope.list.resources){
+        //     $scope.item.resources = $scope.list.resources;
+        // }
+        $scope.saveRecord($scope.item);
+    };
+
+    $scope.cancelModal = function(){
+        modalInstance.close();
+    }
+
+}]);
