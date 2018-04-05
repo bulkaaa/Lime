c9d73f63f6c9b3e74c6edbc4e71c91b9e9d0ee75
Jakub Lewandowski <j.lewandowsk@samsung.com>
2018-04-05 16:48:29 +0200

    displaying formulas in product view

--- src/main/webapp/modals/ViewRecordController.js
+++ src/main/webapp/modals/ViewRecordController.js
@@ -1,6 +1,7 @@
-app.controller('ViewRecordController',  ['$scope', '$http', '$uibModalInstance', 'item', function($scope, $http, modalInstance, item) {
+app.controller('ViewRecordController',  ['$scope', '$http', '$uibModalInstance', 'item', 'formulas', function($scope, $http, modalInstance, item, formulas) {
 	function init(){
         $scope.item = item;
+        $scope.formulas = formulas;
         $scope.cancelModal = function(){
                    modalInstance.close();
         }
