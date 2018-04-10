bb29ab73d2bff41d99b21bece3b3d9d590e48f3a
Maciej_Glowala <vaeppe@gmail.com>
2018-04-10 23:46:08 +0200

    change checkbox to radio button

--- src/main/webapp/modals/AddRecordController.js
+++ src/main/webapp/modals/AddRecordController.js
@@ -1,31 +1,29 @@
 app.controller('AddRecordController',  ['$scope', '$http','$uibModalInstance', function($scope, $http, modalInstance) {
-	$scope.saveEmp = function () {
-		$scope.newRecord = {};
+    $scope.saveEmp = function () {
+        $scope.newRecord = {};
 
-		$scope.cancelModal();
-
-         if($scope.list && $scope.list.roles){
+        $scope.cancelModal();
+        if($scope.list && $scope.list.roles){
             $scope.item.roles = $scope.list.roles;
-         }
+        }
 
-         if($scope.list && $scope.list.products){
-             $scope.item.product = $scope.list.products[0];
-         }
+        if($scope.list && $scope.list.products){
+            $scope.item.product = angular.element(document.getElementById('radioId')).scope().radio.product;
+        }
 
-         if($scope.list && $scope.list.suppliers){
-             $scope.item.supplier = $scope.list.suppliers[0];
-         }
+        if($scope.list && $scope.list.suppliers){
+            $scope.item.supplier = $scope.list.suppliers[0];
+        }
 
-         if($scope.item.startDate && $scope.item.endDate){
-               $scope.item.startDate = new Date($scope.item.startDate).getTime();
-               $scope.item.endDate = new Date($scope.item.endDate).getTime();
-         }
+        if($scope.item.startDate && $scope.item.endDate){
+            $scope.item.startDate = new Date($scope.item.startDate).getTime();
+            $scope.item.endDate = new Date($scope.item.endDate).getTime();
+        }
 
-		$scope.saveRecord($scope.item);
-	 };
+        $scope.saveRecord($scope.item);
+    };
 
-	 $scope.cancelModal = function(){
-         modalInstance.close();
-     }
-
+    $scope.cancelModal = function(){
+        modalInstance.close();
+    }
 }]);
