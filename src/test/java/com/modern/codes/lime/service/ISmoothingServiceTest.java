c51814209c4fcf7eb2ffac3b90ec4f4f604aef1b
Maciej_Glowala <vaeppe@gmail.com>
2018-04-12 22:06:16 +0200

    Test template + repaired session timeout

---
+++ src/test/java/com/modern/codes/lime/service/ISmoothingServiceTest.java
@@ -0,0 +1,38 @@
+package com.modern.codes.lime.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.modern.codes.lime.class_models.TimeSeries;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+@ComponentScan(basePackages = {"com.modern.codes.lime.service"})
+public class ISmoothingServiceTest {
+
+    @Test
+    public void calculateSmoothingPositiveTest(){
+        //given
+        final TimeSeries givenTimeSeries = new TimeSeries();
+
+        //Dodaj co potrzebujesz do tego obiektu givenTimeSeries.
+
+        //when
+        final TimeSeries resultTimeSeries = service.calculateSmoothing(givenTimeSeries, 5);
+
+        //then
+
+        // porównaj wszystkie komponenty które miały się zmienić za pomocą tej metody.
+
+        assertEquals(2, resultTimeSeries.size());
+    }
+
+    @Autowired
+    private ISmoothingService service;
+}
