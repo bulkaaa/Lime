3c4cfd6f45a5caa582f920595406c956461522d0
jaroszk <klaudiajarosz1@poczta.onet.pl>
2018-04-17 00:34:22 +0200

    reset password, roles fixes

--- src/main/webapp/account/reset/ResetPasswordController.js
+++ src/main/webapp/account/reset/ResetPasswordController.js
@@ -2,9 +2,19 @@ angular
         .module('myApp')
         .controller('ResetPasswordController', ResetPasswordController);
 
-   ResetPasswordController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function ResetPasswordController(UserService, $location, $rootScope, FlashService) {
+   ResetPasswordController.$inject = ['UserService', '$location', '$rootScope', 'FlashService', '$scope', '$http', 'dialogs', 'DialogService'];
+    function ResetPasswordController(UserService, $location, $rootScope, FlashService, $scope, $http, $dialogs, DialogService) {
 
-
+    $scope.resetPassword = function(email){
+       $http.delete("/user/change_password/"  + email + "/")
+       .then(
+               function(response){
+                    $dialogs.notify('Email send!', 'Email with your new password has been send successfully');
+               },
+               function(response){
+                   DialogService.generalServerError();
+               }
+       );
+    }
 
     }
