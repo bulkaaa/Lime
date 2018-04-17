6bf926877b14a016c2d202f4f39facae4f24d79d
Klaudia Jarosz <k.jarosz@samsung.com>
2018-04-17 14:24:08 +0200

    reset password fixes

--- src/main/webapp/account/reset/ResetPasswordController.js
+++ src/main/webapp/account/reset/ResetPasswordController.js
@@ -4,15 +4,19 @@ angular
 
    ResetPasswordController.$inject = ['UserService', '$location', '$rootScope', 'FlashService', '$scope', '$http', 'dialogs', 'DialogService'];
     function ResetPasswordController(UserService, $location, $rootScope, FlashService, $scope, $http, $dialogs, DialogService) {
+    var vm = this;
 
     $scope.resetPassword = function(email){
+       vm.dataLoading = true;
        $http.delete("/user/change_password/"  + email + "/")
        .then(
                function(response){
                     $dialogs.notify('Email send!', 'Email with your new password has been send successfully');
+                    vm.dataLoading = false;
                },
                function(response){
-                   DialogService.generalServerError();
+                   $dialogs.notify('Wrong email', 'There is no such email address in our database');
+                   vm.dataLoading = false;
                }
        );
     }
